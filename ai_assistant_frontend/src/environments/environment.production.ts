05224457416835002da9c1124698a9429a599874
moemen20 <103837703+moemen20@users.noreply.github.com>
2026-05-01 10:50:54 +0100

    Delete ai_assistant_frontend directory

--- ai_assistant_frontend/src/environments/environment.production.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiBaseUrl: 'http://localhost:8008',
-  stockApiBaseUrl: 'http://localhost:8009'
-};
-
--- ai_assistant_frontend/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  apiBaseUrl: 'http://localhost:8008',
-  stockApiBaseUrl: 'http://localhost:8009'
-};
-
